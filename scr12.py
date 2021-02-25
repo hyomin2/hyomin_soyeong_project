0c56082d37aaa359ef9188f72b037038783252f0
hyomin2 <60458642+hyomin2@users.noreply.github.com>
2021-02-25 16:35:12 +0900

    Update scr12.py

--- scr12.py
+++ scr12.py
@@ -1,14 +1,6 @@
 import os
 import time
 
-os.system('cd {}'.format(os.getcwd()))
-output = os.popen('pip install pymysql').read()
-print(output)
-
-os.system('cd {}'.format(os.getcwd()))
-output = os.popen('conda install -c scrapinghub scrapy').read()
-print(output)
-
 while True:
     os.system('cd {}'.format(os.getcwd()))
     output = os.popen('scrapy crawl mybots').read()
